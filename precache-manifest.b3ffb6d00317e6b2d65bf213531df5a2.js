b8e52fd87b98fc97539b735a17e421c72b670402
Sindu Sridharan <ssri913@aucklanduni.ac.nz>
2021-02-11 08:53:56 +1300

    Updates

---
+++ precache-manifest.b3ffb6d00317e6b2d65bf213531df5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4104568dd021170b7051ac1256bac256",
+    "url": "/react-voice-ai-app/index.html"
+  },
+  {
+    "revision": "3e6748d461fbc19b52dd",
+    "url": "/react-voice-ai-app/static/css/main.058b1168.chunk.css"
+  },
+  {
+    "revision": "f85365010566b156e045",
+    "url": "/react-voice-ai-app/static/js/2.ad13ac99.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-voice-ai-app/static/js/2.ad13ac99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6748d461fbc19b52dd",
+    "url": "/react-voice-ai-app/static/js/main.ab38bb1a.chunk.js"
+  },
+  {
+    "revision": "6ce67af898e12c61c28f",
+    "url": "/react-voice-ai-app/static/js/runtime-main.ab758a3f.js"
+  }
+]);
